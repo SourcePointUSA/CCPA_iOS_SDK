533a69131bfe89497b4dc2c5f76c0545e1b807ef
Vilas <Vilasma@cybage.com>
2020-01-07 12:48:06 +0530

    Carthage support

---
+++ Example/build/Pods.build/Debug-iphoneos/CCPAConsentViewController.build/Objects-normal/armv7/CCPAConsentViewController-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Vilas/CCPA_SDK/Example/Pods/Target\ Support\ Files/CCPAConsentViewController/CCPAConsentViewController-dummy.m \
+  /Vilas/CCPA_SDK/Example/build/Pods.build/Debug-iphoneos/CCPAConsentViewController.build/module.modulemap \
+  /Vilas/CCPA_SDK/Example/Pods/Target\ Support\ Files/CCPAConsentViewController/CCPAConsentViewController-prefix.pch
---
+++ Example/build/Pods.build/Debug-iphoneos/CCPAConsentViewController.build/Objects-normal/armv7/CCPAConsentViewController_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Vilas/CCPA_SDK/Example/build/Pods.build/Debug-iphoneos/CCPAConsentViewController.build/DerivedSources/CCPAConsentViewController_vers.c
