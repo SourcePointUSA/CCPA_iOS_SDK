df9bafb5efb89aefa6c3a5d9f93b8b71d2da383c
vilasma <vilasma@cybage.com>
2020-05-28 01:44:40 +0530

    Added test cases for MessageWebViewController and MessageViewController

---
+++ Example/CCPAConsentViewController_ExampleTests/MessageViewControllerSpec.swift
@@ -0,0 +1,35 @@
+//
+//  MessageViewControllerSpec.swift
+//  CCPAConsentViewController_ExampleTests
+//
+//  Created by Vilas on 26/05/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import CCPAConsentViewController
+
+class MessageViewControllerSpec: QuickSpec {
+
+    override func spec() {
+        let messageViewController = MessageViewController()
+
+        describe("Test MessageUIDelegate methods") {
+
+            context("Test loadMessage delegate method") {
+                it("Test MessageViewController calls loadMessage delegate method") {
+                    let WRAPPER_API = URL(string: "https://wrapper-api.sp-prod.net")!
+                    messageViewController.loadMessage(fromUrl: WRAPPER_API)
+                    expect(messageViewController).notTo(beNil(), description: "loadMessage delegate method calls successfully")
+                }
+            }
+            context("Test loadPrivacyManager delegate method") {
+                it("Test MessageViewController calls loadPrivacyManager delegate method") {
+                    messageViewController.loadPrivacyManager()
+                    expect(messageViewController).notTo(beNil(), description: "loadPrivacyManager delegate method calls successfully")
+                }
+            }
+        }
+    }
+}
