d4fa59e0d4485899d78e62da6387ecc6cdbf3ba9
Vrushali Deshpande <vrushalid@cybage.com>
2020-10-22 17:49:15 +0530

    [SP-5192] Add UI tests for MetaApp

--- Example/SourcePointMetaApp/AppDelegate/AppDelegate.swift
+++ Example/SourcePointMetaApp/AppDelegate/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CCPAConsentViewController
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -28,6 +29,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if CommandLine.arguments.contains("-cleanAppsData") {
+            CCPAConsentViewController.clearAllConsentData()
+        }
         return true
     }
     
